a0b6aa36d39f9dcdc99211c6e68c04c0203d848d
UssCringe <david.petur@gmail.com>
2022-11-25 13:12:48 +0100

    smt

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom"
+import { Typography, Card, CardContent, CardMedia } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, 
+        demoChannelUrl, demoChannelTitle } from "../utils/constants"
+
+const VideoCard = ({ video: { id: { videoId }, snippet} }) => {
+    //console.log(video)
+    //console.log(videoId, snippet)
+    console.log(snippet.title? snippet.title : "NOTHING FOUND!!!!!!!!!!!!!!!")
+
+  return (
+    <Card sx={{ width: { xs: "320px", sm: "358px", md: "320px" }, boxShadow: "none", borderRadius: 0}}>
+
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+            image={snippet?.thumbnails?.high?.url || demoThumbnailUrl
+            /*question marks are for avoiding errors*/}
+            alt={snippet.title}
+            sx={{ width: {
+              xs: "320px", sm: "358px", md: "320px"
+            }, height: 180 }}
+            />
+      </Link> 
+
+      <CardContent sx={{ background: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+            <Typography variant="subtitle2" fontWeight="bold" color="gray">
+                {snippet?.channelTitle || demoChannelTitle}
+                <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }}></CheckCircle>
+            </Typography>
+        </Link>
+       </CardContent>
+
+    </Card>
+  )
+}
+
+export default VideoCard
---
+++ src/components/Videos.jsx
@@ -0,0 +1,23 @@
+import { Stack, Box } from "@mui/material"
+import { ChannelCard, VideoCard } from "./"
+
+
+const Videos = ({ videos, direction }) => {
+  // console.log(videos) comes up twice
+
+  console.log(videos)
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
+
+      {videos.map((item, index) => (
+        !item.id.playlistId && <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+
+    </Stack>
+  )
+}
+
+export default Videos
